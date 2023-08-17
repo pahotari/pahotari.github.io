0ed856ff7987c5de82bd7c2401583ee1278bfb12
pahotari <pasi.hotari@gmail.com>
2023-08-17 09:50:19 +0300

    updates

--- netlify/functions/influxdb-proxy.js
+++ netlify/functions/influxdb-proxy.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
-    const token = "YOUR_INFLUXDB_TOKEN"; // Replace with your InfluxDB token
+    const token = "YSuHqMz2o57LX3IBvQqY3NM89_z9p15dQqKb9My-3cP_foL8V9_NLEcMX_zOjaJDaWhrmnGtoyK1fb2wLuWhGUQ==";
     const bucket = "iotbucket_cloud_stream";
     const org = "iothot_cloud";
     const influxUrl = "https://europe-west1-1.gcp.cloud2.influxdata.com/api/v2/write?bucket=" + bucket + "&org=" + org + "&precision=s";
